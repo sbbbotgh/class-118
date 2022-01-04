de07f67952d3ae3edfa4f29e321e53174042c886
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2022-01-04 15:27:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    canvas.background("whitesmoke");
+}
+
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("whitesmoke");
+}
